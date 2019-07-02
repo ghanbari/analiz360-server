cd9c5eb034e5459698bc9c9fb3dafb31bebe8c07
Mohammad Ghanbari <ghanbari.daniel@gmail.com>
2019-07-02 12:53:22 +0430

    set project directory for "docker-compose" commands

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -4,5 +4,5 @@ if [[ ! ${PROJECT_DIR} ]]; then
     read -rp "Project directory is not defined, where is it?: " PROJECT_DIR
 fi
 
-docker-compose -f ${PROJECT_DIR}/docker-compose.yml -f ${PROJECT_DIR}/docker-compose.prod.yml build
-docker-compose -f ${PROJECT_DIR}/docker-compose.yml -f ${PROJECT_DIR}/docker-compose.prod.yml up --no-deps -d
+docker-compose -f ${PROJECT_DIR}/docker-compose.yml -f ${PROJECT_DIR}/docker-compose.prod.yml --project-directory=${PROJECT_DIR} build
+docker-compose -f ${PROJECT_DIR}/docker-compose.yml -f ${PROJECT_DIR}/docker-compose.prod.yml --project-directory=${PROJECT_DIR} up --no-deps -d
