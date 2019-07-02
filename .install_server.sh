e1c6cf639ce6aa5b10a9244a41d43d594ddb4c27
Mohammad Ghanbari <ghanbari.daniel@gmail.com>
2019-07-02 12:48:22 +0430

    install swap on installing server

--- .install_server.sh
+++ .install_server.sh
@@ -19,6 +19,28 @@ ufw allow 1338
 ufw --force enable
 ## end block: activate ubuntu firewall
 
+## begin block: install swap
+read -p "Would you like to install swap: " -rn 1 WL_INSTALL_SWAP
+echo
+if [[ ${WL_INSTALL_SWAP} =~ ^[Yy]$ ]]; then
+    read -p "How much swap do you want(per Gig)?: " SWAP_SIZE
+
+    if [[ ! $(grep -q "swapfile.img" "/etc/fstab") ]]
+    then
+        dd if=/dev/zero of=/swapfile.img bs=1024 count=${SWAP_SIZE}M
+        mkswap /swapfile.img
+
+        echo "#swap" >> /etc/fstab
+        echo "/swapfile.img swap swap sw 0 0" >> /etc/fstab
+
+        swapon /swapfile.img
+    else
+        echo "Entry in fstab exists."
+    fi
+fi
+
+## end block: install swap
+
 ## begin block: generate ssh key
 if [[ ! -f "/root/.ssh/id_rsa" ]]; then
     echo "Generate ssh key: "
